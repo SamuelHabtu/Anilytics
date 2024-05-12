202c37c23a65e6a82a3e51777bc703416dbb8f62
Crush Bate <50185023+batec2@users.noreply.github.com>
2024-05-12 14:20:55 -0600

    moved importing logic to a npm package

--- backend/nodejs-testing/server.js
+++ backend/nodejs-testing/server.js
@@ -1,11 +1,12 @@
-import CsvToPostgres from "./src/csv.js";
 import dotenv from "dotenv";
+import CsvToDb from "@batec2/csvtodb";
 import {
   watching_status,
   anime,
   animelist,
   rating_complete,
 } from "./src/tables.js";
+
 dotenv.config();
 
 const config = {
@@ -16,11 +17,11 @@ const config = {
   database: process.env.DB,
 };
 
-const parser = new CsvToPostgres(config);
+const parser = new CsvToDb(config);
 
 // Uncomment one at a time and run, still need to fix so all can be run sequentially, something with promises
 try {
-  // await parser.read("../data/watching_status.csv", watching_status);
+  await parser.read("../data/watching_status.csv", watching_status);
   // await parser.read("../data/anime.csv", anime);
   // await parser.read("../data/rating_complete.csv", rating_complete);
   // await parser.read("../data/animelist.csv", animelist);
